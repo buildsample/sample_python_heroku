6a4b3e0a1f5cb416096c84100b1ad2d41b61545e
Vidya Raghavan <vidya@shippable.com>
2014-09-19 20:48:47 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,10 +8,7 @@ class TestSuite(unittest.TestCase):
         app.calculate()
         self.failIf(app.retrieve() != 62)
 
-def test1(self):
-        app = App()
-        app.calculate()
-        self.failIf(app.retrieve() != 50)
+
 
 def main():
     unittest.main()
